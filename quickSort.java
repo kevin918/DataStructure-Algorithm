5c30852af8a30a77db001688d2448013cf85cd92
Kai Ren <renxx055@umn.edu>
2014-11-12 12:10:00 -0600

    quicksort update

--- quickSort.java
+++ quickSort.java
@@ -1,16 +1,20 @@
-import java.util.*;
+import java.util.Arrays;
+import java.io.*;
 
 class quicksort{
 	public static void main(String[] args){
-
+		int[] arr = {2, 3, 1, 7, 5, 6, 20, 8, 4, 9};
+		System.out.println((Arrays.toString(findKSamllest(arr,10))));
 	}
-	public static void quicksort(int[] input, int front, int end){
+	public static void quicksorts(int[] input, int front, int end){
+		
 		if(front>=end) return;
 		int pivot = partition(input,front,end);
-		quicksort(input,front,pivot);
-		quicksort(input,pivot+1,end);
+		quicksorts(input,front,pivot);
+		quicksorts(input,pivot+1,end);
 	}
-	public int partition(int[] input, int front, int end){
+
+	public static int partition(int[] input, int front, int end){
 		int pivot = input[front];
 		while(front < end){
 			while(input[front]<pivot)
@@ -21,9 +25,26 @@ class quicksort{
 		}
 		return front;
 	}
-	public void swap(int[] input, int s, int l){
+	public static void swap(int[] input, int s, int l){
 		int temp = input[s];
 		input[s] = input[l];
 		input[l] = temp;
+	}
+
+	public static int[] findK(int[] input, int front, int end, int k){
+		if(front>=end) return input;
+		int pivot = partition(input,front,end);
+		//System.out.println(pivot);
+		if(k==pivot){
+			return Arrays.copyOfRange(input,0,pivot);
+		}
+		else {
+			if(k<pivot) 
+				return findK(input,front,pivot,k); 
+			return findK(input,pivot+1,end,k);
+		}
+	}
+	public static int[] findKSamllest(int[] input, int k){
+		return findK(input, 0, input.length-1, k);
 	}
 }
